2d2efc8c4987e97dcfd3d15970ee504e4e27926d
thejuggler <thejugglervenom@gmail.com>
2026-07-31 13:14:03 +0530

    commite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // Ensures relative asset paths work seamlessly on GitHub Pages
 })
